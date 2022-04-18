752c8452d0fd3ee67fa180eefbfbcef348e79ec6
AroLeaf <starleafofa@gmail.com>
2022-04-18 21:45:35 +0200

    [fix] remove old event handler

--- config.js
+++ config.js
@@ -2,12 +2,11 @@ import { loader } from '@aroleaf/djs-bot';
 import 'dotenv/config';
 
 const commands = await loader('commands');
-const events = await loader('events');
 
 const config = {
   intents: [1<<0, 1<<1, 1<<9],
   owner: '659488296820408355',
-  commands, events,
+  commands,
   presence: {
     status: 'online',
     activities: [{
--- events/guildMemberAdd.js
+++
@@ -1,3 +0,0 @@
-import { Event } from '@aroleaf/djs-bot';
-
-export default new Event({ event: 'guildMemberAdd' }, async member => member.client.antiraid.call(member.guild.id, member));
