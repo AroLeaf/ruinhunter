b026d431656ee726393c29af09f78990febdd3a3
Leaf <59538202+AroLeaf@users.noreply.github.com>
2022-04-18 22:16:34 +0200

    [fix] mismatched styling

--- commands/stats.js
+++ commands/stats.js
@@ -6,7 +6,7 @@ export default new SlashCommand({
   description: 'stats',
 }, interaction => {
   interaction.reply({
-    content: `**${interaction.client.guilds.cache.size.toLocaleString()}** guilds **|** **${interaction.client.guilds.cache.reduce((a, v) => a + v.memberCount, 0).toLocaleString()}** members\n**${db.data.raids.length.toLocaleString()}** raids **|** ${db.data.raids.reduce((a,v) => a + v.ids.length, 0)} raiders`,
+    content: `**${interaction.client.guilds.cache.size.toLocaleString()}** guilds **| ${interaction.client.guilds.cache.reduce((a, v) => a + v.memberCount, 0).toLocaleString()}** members\n**${db.data.raids.length.toLocaleString()}** raids **| ${db.data.raids.reduce((a,v) => a + v.ids.length, 0)}** raiders`,
     ephemeral: true,
   })
 });
